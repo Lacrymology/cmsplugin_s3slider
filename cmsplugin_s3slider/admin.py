0aba2bfbd51529bfa7b3c7a62a1f17448f0b6612
Tomas Neme <lacrymology@gmail.com>
2014-05-17 16:46:25 +0700

    fix the media to include cms's jqueryui.sortable

--- cmsplugin_s3slider/admin.py
+++ cmsplugin_s3slider/admin.py
@@ -1,4 +1,5 @@
-from inline_ordering.admin import OrderableStackedInline
+from cms.utils import cms_static_url
+from inline_ordering.admin import OrderableStackedInline, INLINE_ORDERING_JS
 import forms
 import models
 
@@ -14,3 +15,15 @@ class ImageInline(OrderableStackedInline):
             return db_field.formfield(**kwargs)
         return super(ImageInline, self).\
             formfield_for_dbfield(db_field, **kwargs)
+
+    @property
+    def media(self):
+        js = [cms_static_url(path) for path in (
+            'js/libs/jquery.ui.core.js',
+            'js/libs/jquery.ui.sortable.js',
+            )]
+        media = super(ImageInline, self).media
+        if js[-1] not in media._js:
+            ix = media._js.index(INLINE_ORDERING_JS)
+            media._js[ix:ix] = js
+        return media
